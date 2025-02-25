5908fafea7b7ae971a234ec4aa60ffca8ae25307
KSH7-7 <ssktf@naver.com>
2025-02-25 16:32:57 +0000

    fix: html ing-site

---
+++ assets/js/data/swconf.js
@@ -0,0 +1,1 @@
+const swconf = { cacheName: 'chirpy-1740501119',resources: [ '/assets/css/jekyll-theme-chirpy.css', '/', '/categories/', '/tags/', '/archives/', '/about/', '/test/', ], interceptor: {paths: [ ],urlPrefixes: [ ] }, purge: false };
